d61c81d4e41c91b06a8148580c41a312453929f0
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-15 14:50:36 +0530

    first program: Sum of two number

---
+++ 01_VariablesAndDatatypes/05_SumOfTwoNumber.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  // code here
+  int a, b;
+  cout<<"Enter the two numbers: "<<endl;
+  cin >> a >> b;
+
+  int sum = a + b;
+
+  cout << sum;
+
+  return 0;
+}
