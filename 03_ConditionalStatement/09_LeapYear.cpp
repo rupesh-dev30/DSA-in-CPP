d72f8264290a68c5cd9ebcc15c07d5fa92d47571
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-15 18:28:24 +0530

    leap year

---
+++ 03_ConditionalStatement/09_LeapYear.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int year;
+  cout << "Enter the year : ";
+  cin >> year;
+
+  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+    cout << "It is a leap year";
+  } else {
+    cout << "It is not a leap year";
+  }
+  
+  return 0;
+}
