90ec20888aaf164cf4a2cb95a098958acf1eb5b3
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-15 17:01:38 +0530

    practice questions

---
+++ 02_Operators/03_Practice.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  // code here
+  cout << (bool)(3 + 2) << endl;
+  cout << (bool)3 + 2 << endl;
+
+  cout << (23.5 + 2 + 'A') << endl;
+
+  return 0;
+}
