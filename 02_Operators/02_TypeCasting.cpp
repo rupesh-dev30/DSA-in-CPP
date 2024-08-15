3c78dbf5393afe18e51d38852777754cb0e6dd35
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-15 16:55:09 +0530

    typecasting

---
+++ 02_Operators/02_TypeCasting.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  // code here
+
+  // Implicit:
+  cout << 10 / 3 << endl;
+  cout << 10 / 3.0 << endl;
+  cout << 'A' + 1 << endl;
+  cout << "a" - 0 << endl;
+
+  // Explicit:
+  float pi = 3.14;
+  cout << (int)('A') << endl;
+  cout << (int)(pi) << endl;
+
+  cout << ((float)10 / 3) << endl;
+
+  return 0;
+}
+
+/*
+  Typecasting:
+
+    Implicit conversion: (Automatic/ type promotion). Done by compiler to avoid data loss
+
+    Typepromotion
+
+    BOOL -> CHAR -> INT -> FLOAT -> DOUBLE
+
+
+    Explicit conversion: forced bt programmer
+*/
