0dcda7f75cc8cf7576ace1414a98f0c2406e1fa8
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-15 14:25:14 +0530

    boiler plate and introduction of c++ code line wise

---
+++ 01_VariablesAndDatatypes/01_BoilerPlate.cpp
@@ -0,0 +1,22 @@
+#include <iostream>  // All preprocessor directives start with #
+
+#define PI 3.14  //Macros : Symbolic constant
+
+using namespace std;  //Standard C++ Namespace is called std
+
+int main(){   //Starting point of execution
+  cout<<"Namaste Coder"<<endl;
+  cout<<PI;
+
+  return 0;
+}
+
+/* 
+    #How code actually run in c++
+
+        code.cpp   ->   Compiler   ->   code.exe   ->    "Namaste Coders"
+
+    # Header file include pre-written code that's required to execute out program. Ex: iostream, vector, etc
+*/
+
+ 
