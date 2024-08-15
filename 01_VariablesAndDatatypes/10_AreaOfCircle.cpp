ce7188d91bf5fe9bf478cbe6702364e2c4653055
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-15 15:09:07 +0530

    Area of the circle and macros

---
+++ 01_VariablesAndDatatypes/10_AreaOfCircle.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define PI 3.14
+
+using namespace std;
+
+int main() {
+    // code here
+    float radius;
+    cout<<"Enter the radius of the circle: ";
+    cin>>radius;
+
+    float area = PI * radius * radius;
+
+    cout<<"Area of the circle is : "<<area;
+
+    return 0;
+}
