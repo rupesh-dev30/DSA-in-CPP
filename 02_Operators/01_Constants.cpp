2a13dedc410a1ac56f053a0ca64bfecad327e3df
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-15 15:14:12 +0530

    constants variable

---
+++ 02_Operators/01_Constants.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // code here
+    string name = "Rupesh";
+    cout << name << endl;
+
+    name = "Ritesh";
+    cout<<name<<endl;
+
+    const string name1 = "Rupesh";
+    cout << name1 << endl;
+
+    return 0;
+}
+
+
+//Constant are the variables that cannot be changed
