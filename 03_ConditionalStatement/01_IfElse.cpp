9269318ded81198a03d2bb56b91ce9e4c034ca44
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-15 17:40:49 +0530

    if else statement

---
+++ 03_ConditionalStatement/01_IfElse.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int age = 4;
+
+  if (age >= 18) {
+    cout << "You are an adult." << endl;
+  } else {
+    cout << "You are a minor." << endl;
+  }
+
+  return 0;
+}
