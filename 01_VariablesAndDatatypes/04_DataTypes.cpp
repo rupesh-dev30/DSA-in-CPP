8f5f2ac176d0cad70e15559848b3c9ba92f2311f
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-15 14:48:20 +0530

    data types

---
+++ 01_VariablesAndDatatypes/04_DataTypes.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // code here
+    
+
+    return 0;
+}
+
+
+/*
+  Types of Data Types in C++
+    
+    1) Primitive Data Types:
+
+        Integer (int) (4 Bytes): Stores whole numbers (e.g., 10, -5).
+
+        Character (char) (1 Byte): Stores single characters (e.g., 'A', 'b'). Internally, it stores ASCII values.
+
+        Boolean (bool) (1 Byte): Stores true or false.
+
+        Floating-point (float) (4 Bytes): Stores decimal numbers with single precision (e.g., 3.14).
+
+        Double (double) (8 Bytes): Stores decimal numbers with double precision (e.g., 2.71828).
+
+        Void (void): Represents the absence of a type, typically used for functions that do not return a value.
+
+
+    2) Derived Data Types:
+
+        Array: A collection of elements of the same type (e.g., int arr[5]).
+
+        Pointer: Stores the memory address of another variable (e.g., int* ptr).
+
+        Reference: An alias for another variable (e.g., int& ref = var).
+
+        Function: Functions are also considered data types in C++.
+
+
+    3) User-defined Data Types:
+
+        Structure (struct): A collection of variables of different data types (e.g., struct Person).
+
+        Union (union): Similar to structures but shares memory between members.
+
+        Enumeration (enum): A user-defined type consisting of named integral constants (e.g., enum Color {RED, GREEN, BLUE}).
+
+        Class: A blueprint for creating objects, encapsulating data and functions (e.g., class Car).
+
+
+*/
