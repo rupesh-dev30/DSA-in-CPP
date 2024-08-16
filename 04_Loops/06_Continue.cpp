2301e07aa666ce36d4fba7cbadf936ccffa87b2f
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-16 10:57:15 +0530

    continue statement

---
+++ 04_Loops/06_Continue.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // code here
+  for(int i=0; i<=10; i++){
+    if(i%2 != 0){
+      continue;
+    }
+    cout<<i<<" ";
+  }
+
+  return 0;
+}
