4d3cc7c7717ff5e6b3e23b87f72f5d241ac0b9de
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-16 10:55:43 +0530

    break statement

---
+++ 04_Loops/05_Break.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // code here
+  for(int i=0; i<=10; i++){
+    if(i == 5){
+      break;
+    }
+    cout<<i<<" ";
+  }
+
+  return 0;
+}
