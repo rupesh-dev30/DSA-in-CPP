c0fe4345f4980d1b60cb7675bbbd435f38503b68
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-18 13:58:06 +0530

    Data Type modifier

---
+++ 01_VariablesAndDatatypes/11_DataTypeModifier.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // code here
+  cout << "Interger: " << endl;
+  cout << sizeof(int) << endl;
+  cout << sizeof(long int) << endl;
+  cout << sizeof(long long int) << endl;
+  cout << sizeof(short int) << endl;
+
+  cout << "Double: " << endl;
+  cout << sizeof(double) << endl;
+  cout << sizeof(long double) << endl;
+
+  cout << "Unsigned: " << endl;
+  cout << sizeof(unsigned int) << endl;
+  cout << "Signed: " << endl;
+  cout << sizeof(signed int) << endl;
+
+  return 0;
+}
