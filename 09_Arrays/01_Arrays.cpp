6fd3d02d96d0bb563ac9c0c4847b5014948d1254
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-19 15:08:34 +0530

    Array and creating an array

---
+++ 09_Arrays/01_Arrays.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // code here
+
+  // Creating an arrays
+  int a[50];
+
+  int b[50] = {1, 2, 3};
+
+  int c[] = {1, 2, 3};
+
+  cout << a << endl;
+  cout << b[3] << endl;
+  cout << b[1] << endl;
+  cout << c << endl;
+
+  return 0;
+}
+
+/*
+    Arrays:
+      Linear collection of same type of elements that are stored together in contigous memory spaces;
+*/
