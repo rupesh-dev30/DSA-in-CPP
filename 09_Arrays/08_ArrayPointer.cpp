2b0dd6940a2dd645361d5b70b73803272f42835e
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-19 16:13:40 +0530

    Array Pointer

---
+++ 09_Arrays/08_ArrayPointer.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // code here
+  // int x = 10;
+  // int *ptr = &x;
+
+  // int y = 25;
+  // ptr = &y;
+
+  // cout<<*ptr;
+
+  int arr[5];
+  cout<<arr<<"\n";
+
+  int y = 25;
+  //expression must be a modifiable lvalue
+  // arr = &y;
+  
+  return 0;
+}
