291644b0493b7b0e31d7cf4aaacfe5316beae132
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-19 13:10:06 +0530

    pointers

---
+++ 08_Pointers/02_Pointers.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // code here
+  int a = 100;
+  int *ptr = &a; // Asteric is important to store the address, without this we cannot store the address.
+
+  // int p = &a; //this is wrong
+
+  cout << ptr << endl;  // this variable print the addresss...
+  cout << *ptr << endl; // this give the value which is present in the address of &a or ptr
+
+  float pi = 3.14;
+  float *ptr1 = &pi;
+  cout << ptr1 << endl; 
+  cout << *ptr1 << endl; 
+
+  return 0;
+}
