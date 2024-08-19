5593b6b701b67eafca0068e1c8ae3599072a0174
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-19 14:52:50 +0530

    fix comment

--- 08_Pointers/08_Practice.cpp
+++ 08_Pointers/08_Practice.cpp
@@ -6,9 +6,9 @@ int main() {
   int x =5, y = 10;
   int *ptr1 = &x;
   int *ptr2 = &y;
-  
+
   cout << *ptr1 << endl; // prints 5
-  cout << *ptr2 << endl; // prints 5
+  cout << *ptr2 << endl; // prints 10
 
   ptr2 = ptr1;
   cout << *ptr1 << endl; // prints 5
