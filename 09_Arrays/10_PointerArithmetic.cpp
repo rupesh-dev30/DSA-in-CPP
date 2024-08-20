1ee04bef0acce2ccd9dd31f7fb6bd1127d891bdb
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-20 23:53:29 +0530

    pointer arithmetic 2

--- 09_Arrays/10_PointerArithmetic.cpp
+++ 09_Arrays/10_PointerArithmetic.cpp
@@ -1,6 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void printArr(int *ptr, int n) {
+  for (int i = 0; i < n; i++) {
+    cout << *ptr << endl;
+    ptr = ptr + 1;
+  }
+}
+
 int main() {
   // code here
   int a = 5;
@@ -8,9 +15,12 @@ int main() {
 
   cout << ptr << endl;
   cout << (ptr + 1) << endl;
-  ptr = ptr+1;
-
+  ptr = ptr + 1;
   cout << (ptr - 1) << endl;
+
+  int arr[] = {1, 2, 3, 4, 5};
+  int n = sizeof(arr) / sizeof(int);
+  printArr(arr, n);
 
   return 0;
 }
