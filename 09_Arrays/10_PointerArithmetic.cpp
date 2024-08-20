fe427b865a20f1ca0486b0e9ca38ce20dc1f8acb
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-20 23:46:49 +0530

    pointer arithmetic

---
+++ 09_Arrays/10_PointerArithmetic.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // code here
+  int a = 5;
+  int *ptr = &a;
+
+  cout << ptr << endl;
+  cout << (ptr + 1) << endl;
+  ptr = ptr+1;
+
+  cout << (ptr - 1) << endl;
+
+  return 0;
+}
+
+/*
+    Pointer Arithmetic
+
+    Addition and Subtraction of constants
+*/
