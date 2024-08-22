9c323674bb0f4b3e97e25db0d9cee0dca1b39a89
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-23 02:02:28 +0530

    Matrix pointer example

--- 12_2DArrays/08_MatrixPointersinFunctions.cpp
+++ 12_2DArrays/08_MatrixPointersinFunctions.cpp
@@ -1,16 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int matrix(int mat[4][4]) {
-  // ...
+void matrix(int mat[][4], int n, int m) {
+  cout << "0th row ptr " << mat << endl;
+  cout << "1st row ptr " << mat + 1 << endl;
+  cout << "2nd row ptr " << mat + 2 << endl;
+
+  cout << endl;
+
+  cout << "0th row value " << *mat << endl;
+  cout << "1st row value " << *(mat + 1) << endl;
+  cout << "2nd row value " << *(mat + 2) << endl;
+
+  cout << endl;
+
+  cout << *(*(mat + 2) + 2);
 }
 
-int matrixPointer(int (*ptr)[4]) {
+int matrixPointer(int (*mat)[4]) {
   // ...
 }
 
 int main() {
   // code here
+  int mat[4][4] = {
+      {1, 2, 3, 4},
+      {5, 6, 7, 8},
+      {9, 10, 11, 12},
+      {13, 14, 15, 16}};
+
+  matrix(mat, 4, 4);
 
   return 0;
 }
+
+/*
+    ptr[i][j] = *( * ( ptr + i ) + j )
+*/
