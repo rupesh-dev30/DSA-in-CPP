303c953fcb386e3ded33d23ec923590cef40decb
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-23 01:51:37 +0530

    Matrix pointer in functions

---
+++ 12_2DArrays/08_MatrixPointersinFunctions.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int matrix(int mat[4][4]) {
+  // ...
+}
+
+int matrixPointer(int (*ptr)[4]) {
+  // ...
+}
+
+int main() {
+  // code here
+
+  return 0;
+}
