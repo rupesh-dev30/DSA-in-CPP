e6c543375200b0b1761923ee9d721c3380557fa4
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-23 00:32:53 +0530

    Diagonal Sum (brute force)

---
+++ 12_2DArrays/04_DiagonalSum.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int diagonalSum(int arr[][4], int n) {
+  int sum = 0;
+
+  // primary sum
+  //  for(int i=0; i<n; i++){
+  //    sum += arr[i][i];
+  //  }
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (i == j) {
+        sum += arr[i][j];
+      } else if (j == n - i - 1) {
+        sum += arr[i][j];
+      }
+    }
+  }
+  return sum;
+}
+
+int main() {
+  // code here
+  int arr[4][4] = {
+      {1, 2, 3, 4},
+      {5, 6, 7, 8},
+      {9, 10, 11, 12},
+      {13, 14, 15, 16}};
+
+  // Primary Diagonal : 1 , 6, 11, 16
+  // Secondary Diagonal: 4 , 7, 10, 13
+
+  cout << " sum of diagonal element is : " << diagonalSum(arr, 4) << endl;
+
+  int arr2[3][3] = {
+      {1, 2, 3},
+      {5, 6, 7},
+      {9, 10, 11}};
+
+  // Primary Diagonal : 1, 6, 11
+  // Secondary Diagonal: 3, 6, 9       Dupicate (6) : ODD: Middle element come twice
+
+  // cout << " sum of diagonal element is : " << diagonalSum(arr2, 3) << endl;
+
+  return 0;
+}
