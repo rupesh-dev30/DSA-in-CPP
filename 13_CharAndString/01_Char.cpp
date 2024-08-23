0ae5f631bcacb0b0145177f3fd15f4e230069e60
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-23 22:43:07 +0530

    Char 1

---
+++ 13_CharAndString/01_Char.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ASCII : American Standard Code for Information Interchange (7bit character set (128 ch))
+
+int main() {
+  // code here
+  char ch1 = 'a';
+  char ch2 = 'X';
+  char ch3 = '&';
+  char ch4 = '9';
+  char ch5 = '\n';
+
+  cout << ch1 << endl;
+  cout << ch2 << endl;
+  cout << ch3 << endl;
+  cout << ch4 << endl;
+  cout << ch5 << endl;
+
+  cout << (int) 'A' << endl;
+
+  return 0;
+}
+
+/*
+    Char -> 1 Byte
+
+    ASCII Value: 'a' -> 97
+    ASCII Value: 'A' -> 65
+
+*/
