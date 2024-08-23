959da1af84132517b46ae5e01f5dcfca70c21ee4
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-23 22:58:10 +0530

    strlen func

--- 13_CharAndString/03_StringLiterals.cpp
+++ 13_CharAndString/03_StringLiterals.cpp
@@ -6,11 +6,16 @@ int main() {
   char work[] = "code";
   cout << work << endl;
 
-  char work1[5] = "work";  // 1 extra for null character
+  char work1[5] = "work"; // 1 extra for null character
   cout << work1 << endl;
 
-  char work2[10] = "work2";  // it add \0 by default and end it
+  char work2[10] = "work2"; // it add \0 by default and end it
   cout << work2 << endl;
+
+  // char work3[50] = {'c', 'o', 'd', 'e'};
+  char work3[50] = {'c', 'o', 'd', 'e', '\0'};
+  cout << work3 << endl;
+  cout << strlen(work3) << endl;
 
   return 0;
 }
