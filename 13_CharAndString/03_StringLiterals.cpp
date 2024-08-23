702baf56df6f60e97766c98880a3d76358bc06d4
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-23 22:56:17 +0530

    String literals

---
+++ 13_CharAndString/03_StringLiterals.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // code here
+  char work[] = "code";
+  cout << work << endl;
+
+  char work1[5] = "work";  // 1 extra for null character
+  cout << work1 << endl;
+
+  char work2[10] = "work2";  // it add \0 by default and end it
+  cout << work2 << endl;
+
+  return 0;
+}
