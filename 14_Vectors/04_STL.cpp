6cfc04536f839c6b10801f19c23f75cb5b211f2a
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-24 18:19:51 +0530

    Vector

---
+++ 14_Vectors/04_STL.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // code here
+
+  return 0;
+}
+
+/*
+    STL (Standard Template Library): It is a container classes, algorithm & iterators.
+
+    It contains the implementation of common data structures like vectors, stacks, maps etc.
+*/
---
+++ 14_Vectors/05_Vector.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // code here
+  vector<int> v1;
+  vector<int> v2 = {1,2,3,4};
+  vector<int> v3(5, -1);
+  // 5 -> size filled with -1
+  
+  return 0;
+}
+
+// Vectors: Like Dynamic arrays that can resize dynamically at runtime.
+// They are stored contiguously in memory
