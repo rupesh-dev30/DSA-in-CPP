b66625a6f1faa94f602d47520f9d235006c08b9d
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-24 18:24:48 +0530

    vector size capactiy after full

--- 14_Vectors/05_Vector.cpp
+++ 14_Vectors/05_Vector.cpp
@@ -4,10 +4,18 @@ using namespace std;
 int main() {
   // code here
   vector<int> v1;
-  vector<int> v2 = {1,2,3,4};
+  vector<int> v2 = {1, 2, 3, 4};
   vector<int> v3(5, -1);
   // 5 -> size filled with -1
+
+  cout << "size: " << v2.size() << endl;
+  cout << "capacity: "<<v2.capacity()<<endl;
+
+  v2.push_back(5);
   
+  cout << "size: " << v2.size() << endl;
+  cout << "capacity: "<<v2.capacity()<<endl;
+
   return 0;
 }
 
